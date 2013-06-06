8c6dabbd2847c60fefae8ff8038d5c7a7d828b8d
Alex Krapivin <alex.krapivin@lge.com>
2013-06-06 10:36:09 +0400

    Build WebKit script uses QTDIR

--- build-webkit.bash
+++ build-webkit.bash
@@ -1,41 +1,47 @@
 #!/bin/bash
 
-export WEBKITOUTPUTDIR="WebKitBuild/desktop"
-WEBKIT_ROOT=$(pwd)
-export QMAKEPATH=`pwd`"/Tools/qmake"
+if [ ! -d "WebKit" ]; then
+   echo "Could not find WebKit directory"
+   exit 1
+fi
 
-LUNA_STAGING=`pwd`"/../staging"
+if [ ! -d "qtsdk/qtbase" ]; then
+   echo "Could not find qtsdk/qtbase directory"
+   exit 1
+fi
+
+export QTDIR=`pwd`"/qtsdk/qtbase"
+
+cd WebKit
+WEBKIT_ROOT=$(pwd)
+export WEBKITOUTPUTDIR="WebKitBuild/desktop"
+export QMAKEPATH=`pwd`"/Tools/qmake"
+export PATH=$QTDIR/bin:$PATH
+
 JOBS="-j 8"
 
 echo " WEBKIT_ROOT=$WEBKIT_ROOT"
 echo "   QMAKEPATH=$QMAKEPATH"
-echo "LUNA_STAGING=$LUNA_STAGING"
+echo "       QTDIR=$QTDIR"
 echo "        JOBS=$JOBS"
 #exit 0
-
-export QTDIR=$LUNA_STAGING
-export PATH=$LUNA_STAGING/bin:$PATH
-###export WEBKIT_TESTFONTS=$HOME/work/goldfinger/project/upstream-webkit-build-bot/testfonts
-###export TZ=America/Los_Angeles
 
 mkdir -p $WEBKITOUTPUTDIR
 cd $WEBKITOUTPUTDIR
 
-QMAKE=$LUNA_STAGING/bin/qmake
+QMAKE=$QTDIR/bin/qmake
 $WEBKIT_ROOT/Tools/Scripts/build-webkit \
    --qmake=${QMAKE} \
    --qmakearg="QMAKE_CXXFLAGS+=-Wno-unused-variable" \
    --qmakearg="QMAKE_CXXFLAGS+=-Wno-ignored-qualifiers" \
    --qmakearg="QMAKE_CXXFLAGS+=-Wno-deprecated-declarations" \
    --qmakearg="QMAKE_LFLAGS+=-L$WEBKIT_ROOT/$WEBKITOUTPUTDIR/Release/lib" \
-   --qmakearg="QMAKE_RPATHDIR+=$LUNA_STAGING/lib" \
-   --qmakearg="QMAKE_RPATHDIR+=$LUNA_STAGING/usr/lib" \
+   --qmakearg="QMAKE_RPATHDIR+=$QTDIR/lib" \
+   --qmakearg="QMAKE_RPATHDIR+=$QTDIR/usr/lib" \
    --qmakearg="WEBKIT_CONFIG-=palm_service_bridge" \
    --qmakearg="WEBKIT_CONFIG-=use_umediaserver" \
    --qmakearg="DEFINES+=WEBOS_DESKTOP" \
    --qmakearg="WEBKIT_CONFIG-=use_gstreamer"
 
-   #--qmakearg="WEBKIT_CONFIG+=build_qttestsupport" \ [ $? -eq 0 ] || fail "Failed building Webkit"
-
 make $JOBS 
-# && cd $WEBKIT_ROOT/$WEBKITOUTPUTDIR/Release && make install
+#&& cd $WEBKIT_ROOT/$WEBKITOUTPUTDIR/Release && make install
